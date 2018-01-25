1493638d78d8ac34e04b55270060ccde560c3b07
ctaepper <ct@tresmo.de>
2018-01-25 11:27:48 +0100

    initial commit

---
+++ packages/p1/index.js
@@ -0,0 +1,5 @@
+const repeat = require('repeat-string')
+
+module.exports = function repeat5times(subject) {
+  return repeat(subject, 5)
+}
---
+++ packages/p2/index.js
@@ -0,0 +1,5 @@
+const internalRepeat = require('@myscope/p1')
+const repeat = require('repeat-string')
+
+console.log(internalRepeat('try renovate me!'))
+console.log(repeat('is it working?', 3))
